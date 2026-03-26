d8b5eae5ce1718d3478107424d7e7326b820076e
Ayush Tomar <ayushtomar061004@gmail.com>
2026-03-27 00:46:13 +0530

    Fix: Added X-User-Email to CORS allowed headers to resolve missing data issues

--- quickcombo/settings.py
+++ quickcombo/settings.py
@@ -91,6 +91,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default="http://localhost:3000,http://127.0.0.1:3000").split(',')
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-user-email',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
